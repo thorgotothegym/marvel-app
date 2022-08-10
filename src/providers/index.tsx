ad47be5c46b72300ef0362f7e70a08dd96f7c15a
Aleix Vidal <al3ix.vidal@gmail.com>
2022-08-10 10:19:23 +0200

    feat: implement providers for testing

---
+++ src/providers/index.tsx
@@ -0,0 +1,31 @@
+import React, { PropsWithChildren } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryClientProviderProps,
+} from "react-query";
+
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true,
+      onError: () => {
+        console.log("error");
+      },
+    },
+  },
+});
+
+export const QueryProvider = ({
+  children,
+  ...props
+}: PropsWithChildren<Partial<QueryClientProviderProps>>) => {
+  return (
+    <QueryClientProvider {...props} client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
