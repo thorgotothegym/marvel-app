e1d702b191fa4a92a81ac289dad50f8e1db44c18
Aleix Vidal <al3ix.vidal@gmail.com>
2022-08-10 09:30:34 +0200

    feat: remove row and cols

--- src/pages/CharacterDetails/index.tsx
+++ src/pages/CharacterDetails/index.tsx
@@ -31,186 +31,172 @@ export const CharacterDetails = (): JSX.Element => {
   }, []);
   return (
     <>
-      <Row justify="center" align="middle">
-        {byID.status === "error" ? (
-          <Text type="danger">
-            Wow this is embarrassing... the character you are looking for does
-            not exist. <Link onClick={() => navigate("/")}>Back to list</Link>
-          </Text>
-        ) : null}
-        {byID.isLoading ? (
-          <Col>Loading...</Col>
-        ) : (
-          <>
-            {byID.data?.data.results.map((item) => {
-              return (
-                <div key={item.id}>
-                  <Row>
-                    <Col>
-                      <Button
-                        type="primary"
-                        style={styles.navigation}
-                        onClick={() => navigate(-1)}
-                      >
-                        Go back to the list of characters
-                      </Button>
-                    </Col>
-                  </Row>
-                  <Row justify="center" align="middle">
-                    <Col span={12}>
-                      <Wrapper>
-                        <img
-                          src={
-                            item.thumbnail.path + "." + item.thumbnail.extension
-                          }
-                          alt={item.name}
-                        />
-                      </Wrapper>
-                    </Col>
-                    <Col span={12}>
-                      {item.name ? (
-                        <>
-                          <Typography>
-                            <h4>Who is {item.name}?</h4>
-                          </Typography>
-                        </>
-                      ) : (
-                        <Typography>
-                          Oops, where is not data available
-                        </Typography>
-                      )}
-                      {item.description ? (
-                        <Text type="secondary">{item.description}</Text>
-                      ) : (
-                        <Typography>
-                          Oops... we have no information on the character
-                        </Typography>
-                      )}
-                    </Col>
-                  </Row>
+      {byID.status === "error" ? (
+        <Text type="danger">
+          Wow this is embarrassing... the character you are looking for does not
+          exist. <Link onClick={() => navigate("/")}>Back to list</Link>
+        </Text>
+      ) : null}
+      {byID.isLoading ? (
+        <Col>Loading...</Col>
+      ) : (
+        <>
+          {byID.data?.data.results.map((item) => {
+            return (
+              <div key={item.id}>
+                <Button
+                  type="primary"
+                  style={styles.navigation}
+                  onClick={() => navigate(-1)}
+                >
+                  Go back to the list of characters
+                </Button>
+                <>
+                  <Wrapper>
+                    <img
+                      src={item.thumbnail.path + "." + item.thumbnail.extension}
+                      alt={item.name}
+                    />
+                  </Wrapper>
+                  {item.name ? (
+                    <>
+                      <Typography>
+                        <h4>Who is {item.name}?</h4>
+                      </Typography>
+                    </>
+                  ) : (
+                    <Typography>Oops, where is not data available</Typography>
+                  )}
+                  {item.description ? (
+                    <Text type="secondary">{item.description}</Text>
+                  ) : (
+                    <Typography>
+                      Oops... we have no information on the character
+                    </Typography>
+                  )}
+                </>
 
-                  <Descriptions title="Events">
-                    <Descriptions.Item label="Events available">
-                      {item.events?.available === 0 ? (
-                        <Typography.Text type="danger">
-                          Oops... no events available
-                        </Typography.Text>
-                      ) : (
-                        item.events?.available
-                      )}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="CollectionURI">
-                      <a href={item.events?.collectionURI}>
-                        CollectionURI click here
-                      </a>
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Returned">
-                      {item.events?.returned === 0 ? (
-                        <Typography.Text type="danger">
-                          "Oops... no results"
-                        </Typography.Text>
-                      ) : (
-                        <Text>{item.events?.returned} </Text>
-                      )}
-                    </Descriptions.Item>
-                    {item.events?.items.length === 0 ? null : (
-                      <>
-                        {item.events?.items.map((moreItems, key) => {
-                          return (
-                            <Descriptions.Item key={key.toString()}>
-                              <Text strong>{moreItems.name} </Text>
-                              <br />
-                              <Text italic> {moreItems.resourceURI}</Text>
-                              <Text italic>{moreItems.type}</Text>
-                            </Descriptions.Item>
-                          );
-                        })}
-                      </>
+                <Descriptions title="Events">
+                  <Descriptions.Item label="Events available">
+                    {item.events?.available === 0 ? (
+                      <Typography.Text type="danger">
+                        Oops... no events available
+                      </Typography.Text>
+                    ) : (
+                      item.events?.available
                     )}
-                  </Descriptions>
-                  <Divider plain />
-                  <Descriptions title="Stories">
-                    <Descriptions.Item label="Stories available">
-                      {item.stories?.available === 0 ? (
-                        <Typography.Text type="danger">
-                          Oops... no stories available
-                        </Typography.Text>
-                      ) : (
-                        item.stories?.available
-                      )}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="CollectionURI">
-                      <a href={item.stories?.collectionURI}>
-                        CollectionURI click here
-                      </a>
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Returned">
-                      {item.stories?.returned === 0 ? (
-                        <Typography.Text type="danger">
-                          "Oops... no results"
-                        </Typography.Text>
-                      ) : (
-                        <Text>{item.stories?.returned} </Text>
-                      )}
-                    </Descriptions.Item>
-                    {item.stories?.items.length === 0 ? null : (
-                      <>
-                        {item.stories?.items.map((moreItems, key) => {
-                          return (
-                            <Descriptions.Item key={key.toString()}>
-                              <Text strong>{moreItems.name} </Text>
-                              <Text italic> {moreItems.resourceURI}</Text>
-                              <Text italic>{moreItems.type}</Text>
-                            </Descriptions.Item>
-                          );
-                        })}
-                      </>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="CollectionURI">
+                    <a href={item.events?.collectionURI}>
+                      CollectionURI click here
+                    </a>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Returned">
+                    {item.events?.returned === 0 ? (
+                      <Typography.Text type="danger">
+                        "Oops... no results"
+                      </Typography.Text>
+                    ) : (
+                      <Text>{item.events?.returned} </Text>
                     )}
-                  </Descriptions>
-                  <Divider plain />
-                  <Descriptions title="Series">
-                    <Descriptions.Item label="Series available">
-                      {item.series?.available === 0 ? (
-                        <Typography.Text type="danger">
-                          Oops... no series available
-                        </Typography.Text>
-                      ) : (
-                        item.series?.available
-                      )}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="CollectionURI">
-                      <a href={item.series?.collectionURI}>
-                        CollectionURI click here
-                      </a>
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Returned">
-                      {item.series?.returned === 0 ? (
-                        <Typography.Text type="danger">
-                          "Oops... no results"
-                        </Typography.Text>
-                      ) : (
-                        <Text>{item.series?.returned} </Text>
-                      )}
-                    </Descriptions.Item>
-                    {item.series?.items.length === 0 ? null : (
-                      <>
-                        {item.series?.items.map((moreItems, key) => {
-                          return (
-                            <Descriptions.Item key={key.toString()}>
-                              <Text strong>{moreItems.name} </Text>
-                              <Text italic> {moreItems.resourceURI}</Text>
-                            </Descriptions.Item>
-                          );
-                        })}
-                      </>
+                  </Descriptions.Item>
+                  {item.events?.items.length === 0 ? null : (
+                    <>
+                      {item.events?.items.map((moreItems, key) => {
+                        return (
+                          <Descriptions.Item key={key.toString()}>
+                            <Text strong>{moreItems.name} </Text>
+                            <br />
+                            <Text italic> {moreItems.resourceURI}</Text>
+                            <Text italic>{moreItems.type}</Text>
+                          </Descriptions.Item>
+                        );
+                      })}
+                    </>
+                  )}
+                </Descriptions>
+                <Divider plain />
+                <Descriptions title="Stories">
+                  <Descriptions.Item label="Stories available">
+                    {item.stories?.available === 0 ? (
+                      <Typography.Text type="danger">
+                        Oops... no stories available
+                      </Typography.Text>
+                    ) : (
+                      item.stories?.available
                     )}
-                  </Descriptions>
-                </div>
-              );
-            })}
-          </>
-        )}
-      </Row>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="CollectionURI">
+                    <a href={item.stories?.collectionURI}>
+                      CollectionURI click here
+                    </a>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Returned">
+                    {item.stories?.returned === 0 ? (
+                      <Typography.Text type="danger">
+                        "Oops... no results"
+                      </Typography.Text>
+                    ) : (
+                      <Text>{item.stories?.returned} </Text>
+                    )}
+                  </Descriptions.Item>
+                  {item.stories?.items.length === 0 ? null : (
+                    <>
+                      {item.stories?.items.map((moreItems, key) => {
+                        return (
+                          <Descriptions.Item key={key.toString()}>
+                            <Text strong>{moreItems.name} </Text>
+                            <Text italic> {moreItems.resourceURI}</Text>
+                            <Text italic>{moreItems.type}</Text>
+                          </Descriptions.Item>
+                        );
+                      })}
+                    </>
+                  )}
+                </Descriptions>
+                <Divider plain />
+                <Descriptions title="Series">
+                  <Descriptions.Item label="Series available">
+                    {item.series?.available === 0 ? (
+                      <Typography.Text type="danger">
+                        Oops... no series available
+                      </Typography.Text>
+                    ) : (
+                      item.series?.available
+                    )}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="CollectionURI">
+                    <a href={item.series?.collectionURI}>
+                      CollectionURI click here
+                    </a>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Returned">
+                    {item.series?.returned === 0 ? (
+                      <Typography.Text type="danger">
+                        "Oops... no results"
+                      </Typography.Text>
+                    ) : (
+                      <Text>{item.series?.returned} </Text>
+                    )}
+                  </Descriptions.Item>
+                  {item.series?.items.length === 0 ? null : (
+                    <>
+                      {item.series?.items.map((moreItems, key) => {
+                        return (
+                          <Descriptions.Item key={key.toString()}>
+                            <Text strong>{moreItems.name} </Text>
+                            <Text italic> {moreItems.resourceURI}</Text>
+                          </Descriptions.Item>
+                        );
+                      })}
+                    </>
+                  )}
+                </Descriptions>
+              </div>
+            );
+          })}
+        </>
+      )}
     </>
   );
 };
