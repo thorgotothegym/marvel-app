90fcca7371c4e0b8cadb34fa6b286bd9418a0c02
Aleix Vidal <al3ix.vidal@gmail.com>
2022-08-10 11:07:17 +0200

    feat: add new wrapper

--- src/components/CounterResults/index.test.tsx
+++ src/components/CounterResults/index.test.tsx
@@ -9,11 +9,24 @@ const Wrapper = () => (
   </QueryProvider>
 );
 
+const WrapperFalse = () => (
+  <QueryProvider>
+    <CounterResults total={0} key={1} />
+  </QueryProvider>
+);
+
 describe("CounterResults", () => {
   it("know that the total is true", async () => {
     render(<Wrapper />);
     const totalTrue = screen.getByText(
       "20 matches have been found with your search"
+    );
+    expect(totalTrue).toBeInTheDocument();
+  });
+  it("no results found.", async () => {
+    render(<WrapperFalse />);
+    const totalTrue = screen.getByText(
+      "Oops, no results found. Try another search"
     );
     expect(totalTrue).toBeInTheDocument();
   });
